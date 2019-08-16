efab4bf73e9c3d0ea2c8b99791b3468ae3b5af8e
wangweijian <1030061126@qq.com>
2019-08-16 15:33:03 +0800

    news

--- src/index.js
+++ src/index.js
@@ -1,12 +1,35 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { Component } from 'react';
+import ReactDom from 'react-dom';
+import { Layout } from 'antd';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import AppHeader from './components/Header/';
+import List from './containers/List/';
+import Detail from './containers/Detail/';
+import 'antd/dist/antd.css';
+import './style.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const { Header, Footer, Content } = Layout;
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+class App extends Component {
+	render() {
+		return (
+			<BrowserRouter>
+				<Layout style={{ minWidth: 1260, height: '100%' }}>
+		      <Header className="header">
+		      	<AppHeader />
+		      </Header>
+		      <Content className="content">
+	      		<Switch>
+	      			<Route path='/detail/:id' component={Detail} />
+		      		<Route path='/:id?' component={List} />
+		      	</Switch>
+		      </Content>
+		      <Footer className="footer">@ Vins-wang 2019</Footer>
+	    	</Layout>
+    	</BrowserRouter>
+		)
+	}
+}
+
+
+ReactDom.render(<App />, document.getElementById('root'));
